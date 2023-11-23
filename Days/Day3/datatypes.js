8d4eac1dc3a2fdfd5dc26f4395360cad7c8167c1
Akshay Kumar <akshayjnp2018@gmail.com>
2023-11-24 00:39:51 +0530

    datatypes

---
+++ Days/Day3/datatypes.js
@@ -0,0 +1,24 @@
+//---------------------------------- Day-3 , 24 Nov 2023 -----------------------------------------------------
+
+"use strict";           // this will treat all JS code as newer standard version
+
+
+/*---------------- data types -------------------------------------
+
+1 - number => 2^53(max value)
+2 - boolean => true/false
+3 - string => "Akshay"
+4 - null => it is a standalone value itself.
+5 - undefined => it is undefined, different from null.
+6 - symbol => to define uniqueness
+
+7- object - it is a datatype. null is a object datatype.
+
+*/
+
+let name = "Akshay"
+let age = 24
+let add = undefined
+let mobNo = null
+
+console.table([typeof "name", typeof age , typeof mobNo , typeof add]);
