0d4a8a13016f2fcf6d428d8383d9d97d16406ed2
Akshay Kumar <akshayjnp2018@gmail.com>
2024-07-22 23:47:01 +0530

    Update operations_conversion.js

--- Days/Day4/operations_conversion.js
+++ Days/Day4/operations_conversion.js
@@ -37,8 +37,19 @@ console.log(num2);
 let gameCounter = 100 
 ++gameCounter;
 console.log(gameCounter);
+
 gameCounter++;
 console.log(gameCounter);
+
+let x = 3;
+const y = x++;
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
 
 
 // link to study - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
