7000a45d3dcb5f2960d99ae238d2cb85a786149e
Akshay Kumar <akshayjnp2018@gmail.com>
2024-07-31 02:52:03 +0530

    31-07-2024

--- Exercise/Day10.js
+++ Exercise/Day10.js
@@ -21,4 +21,5 @@ Activity 4: Form Events
 Activity 5: Event Delegation
 * Task 9: Add a click event listener to a list that logs the text content of the clicked list item using event delegation.
 * Task 10: Add an event listener to a parent element that listens for events from dynamically added child elements.
+
 */
